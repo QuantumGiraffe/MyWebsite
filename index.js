ed5d73606f9f89a8b20dbb60f6e20deeb978364d
CosmicGiraffe <99769547+CosmicGiraffe@users.noreply.github.com>
2022-06-06 18:00:51 -0400

    Photography Page Updates
    Added carousel and 14 pictures for the photo roll

---
+++ index.js
@@ -0,0 +1,48 @@
+
+const slides = document.getElementsByClassName('carousel-item');
+let slidePosition = 0;
+const totalSlides = slides.length;
+
+document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide);
+document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide);
+
+function hideAllSlides() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel-item-visible');
+        slide.classList.add('carousel-item-hidden');
+    }
+}
+
+function moveToNextSlide() {
+    hideAllSlides();
+    
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    
+    slides[slidePosition].classList.add("carousel-item-visible");
+    // displaySlideCount();
+}
+
+function moveToPrevSlide() {
+    hideAllSlides();
+    
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    
+    slides[slidePosition].classList.add("carousel-item-visible");
+    // displaySlideCount();
+}
+
+
+// Need to make this functional (want to add a picture display count)
+// function displaySlideCount(){
+//     const numberHolder = document.getElementById("counter")
+//     numberHolder.innerHTML = slidePosition
+//     console.log(slidePosition)
+// }
